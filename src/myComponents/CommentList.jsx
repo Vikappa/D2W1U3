2c391c8d11af5116a322ba76d962459b508680d2
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-01 23:11:31 +0100

    Aggiunto pulsante responsivo senza logica

--- src/myComponents/CommentList.jsx
+++ src/myComponents/CommentList.jsx
@@ -1,37 +1,47 @@
 import ListGroup from 'react-bootstrap/ListGroup';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-
+import { faStar, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+<FontAwesomeIcon icon="fa-regular fa-trash-can" />
 function CommentList(props) {
-  const { comment, author, rate, createdAt, updatedAt } = props.comment
-  const dataDaUsare = updatedAt || createdAt // se non c'è updatedAt, usa createdAt
-  const displayDate = new Date(dataDaUsare).toLocaleDateString("it-IT", {
+  const { comment, author, rate, createdAt, updatedAt } = props.comment;
+  const formattedDate = updatedAt || createdAt;
+
+  // Converti la data in formato leggibile
+  const displayDate = new Date(formattedDate).toLocaleDateString("it-IT", {
     year: 'numeric', month: 'long', day: 'numeric',
     hour: '2-digit', minute: '2-digit'
-  })
+  });
 
+  // Genera le stelle in base al rate
   const renderStars = (rate) => {
-    let stars = []
+    let stars = [];
     for (let i = 0; i < rate; i++) {
-      stars.push(<FontAwesomeIcon icon={faStar} key={i} style={{ color: 'gold' }} />)
+      stars.push(<FontAwesomeIcon icon={faStar} key={i} className="text-warning" />);
     }
-    return stars
-  }
+    return stars;
+  };
 
   return (
     <ListGroup.Item>
-      <div className="d-flex justify-content-between align-items-center">
-        <div className='d-flex flex-column align-items-start d-md-inline'>
+      <div className="d-flex justify-content-between align-items-center position-relative">
+        <div className='d-flex flex-column align-items-start'>
           <span className="text-primary fw-bold" style={{ fontSize: '18px' }}>{author}:</span> {comment}
         </div>
-        <div className="d-none d-md-flex flex-column align-items-end">
-          <div className="text-muted" style={{ fontSize: '14px' }}>{displayDate}</div>
-          <div>{renderStars(rate)}</div>
+        <div className="d-none d-lg-flex flex-column align-items-end  justify-content-between">
+          <div className="text-muted d-flex" style={{ fontSize: '14px' }}>{displayDate}</div>
+          <div className='d-flex align-items-center justify-content-between'> 
+          <FontAwesomeIcon icon={faTrashCan} className='btn btn-sm btn-outline-danger buttonEliminaLg'/>
+            <div className='container-fluid ms-auto'>
+            {renderStars(rate)}
+            </div>
+          </div>
         </div>
+        <button className="btn btn-sm btn-outline-danger buttonEliminaSm">
+        <FontAwesomeIcon icon={faTrashCan} />
+        </button>
       </div>
     </ListGroup.Item>
   );
 }
 
-export default CommentList
-
+export default CommentList;
